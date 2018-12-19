77c53fa3d42726fd78ca5dc541c8123fa5c4ba5a
Jaeden Amero <jaeden.amero@arm.com>
2018-12-19 18:10:22 +0000

    Update check-names to work
    XXX may need to be a fixup to the move headers commit

--- tests/scripts/list-macros.sh
+++ tests/scripts/list-macros.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-if [ -d include/mbedtls ]; then :; else
+if [ -d include/psa ]; then :; else
     echo "$0: must be run from root" >&2
     exit 1
 fi
 
-HEADERS=$( ls include/mbedtls/*.h | egrep -v 'compat-1\.3\.h' )
+HEADERS=$( ls include/psa/*.h | egrep -v 'compat-1\.3\.h' )
 
 sed -n -e 's/.*#define \([a-zA-Z0-9_]*\).*/\1/p' $HEADERS \
     | egrep -v '^(asm|inline|EMIT|_CRT_SECURE_NO_DEPRECATE)$|^MULADDC_' \
