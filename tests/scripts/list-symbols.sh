ffb5c0bad18dec61664db13e3f5b4daed0d6151e
Jaeden Amero <jaeden.amero@arm.com>
2018-12-20 16:17:23 +0000

    fix move config

--- tests/scripts/list-symbols.sh
+++ tests/scripts/list-symbols.sh
@@ -12,10 +12,10 @@ if grep -i cmake Makefile >/dev/null; then
     exit 1
 fi
 
-cp include/mbedtls/config.h include/mbedtls/config.h.bak
+cp include/mbedcrypto/config.h include/mbedcrypto/config.h.bak
 scripts/config.pl full
 CFLAGS=-fno-asynchronous-unwind-tables make clean lib >/dev/null 2>&1
-mv include/mbedtls/config.h.bak include/mbedtls/config.h
+mv include/mbedcrypto/config.h.bak include/mbedcrypto/config.h
 if uname | grep -F Darwin >/dev/null; then
     nm -gUj library/libmbed*.a 2>/dev/null | sed -n -e 's/^_//p'
 elif uname | grep -F Linux >/dev/null; then
