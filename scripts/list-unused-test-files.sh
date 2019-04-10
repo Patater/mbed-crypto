22330299ebd9979edfc091e7efcf5119df2ff94d
Jaeden Amero <jaeden.amero@arm.com>
2019-04-10 17:52:08 +0100

    XXX play with list of unused data files

--- scripts/list-unused-test-files.sh
+++ scripts/list-unused-test-files.sh
@@ -12,3 +12,15 @@ ls tests/data_files | \
     while read f; do
         grep -R -F --exclude=Makefile "$f" tests >/dev/null || echo "$f"
     done
+
+echo "frank"
+
+for f in tests/data_files/*; do
+  grep -q -F "$f" tests/suites/*.data tests/suites/*.function || echo "$f"
+done
+
+echo "magic"
+
+for f in tests/data_files/*; do
+  grep -q -R -F --exclude=tests/data_files/Makefile "$f" tests || echo "$f"
+done
